03a236d86b6f448c279816cf6aca0bcce8fdc861
Dino <dohuy419@gmail.com>
2025-11-26 08:54:55 +0700

    1.0

--- shop toy/shop toy/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Builder;
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.UseDefaultFiles(); // tìm index.html trong wwwroot
-app.UseStaticFiles();
-
-app.Run();
